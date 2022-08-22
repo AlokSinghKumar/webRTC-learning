bdb0e0f45fe9916321e47894dd4e14758c574700
AlokSinghKumar <aloksinghkumar23@gmail.com>
2022-08-22 21:57:22 +0530

    dragable user 1

--- peerChat/main.js
+++ peerChat/main.js
@@ -91,6 +91,7 @@ let createPeerConnection = async (MemberId) => {
     document.getElementById ("user-2").style.display = 'block';
 
     document.getElementById ("user-1").classList.add ("smallFrame");
+    dragElement (document.getElementById ("user-1"))
 
     if(!localStream){
         localStream = await navigator.mediaDevices.getUserMedia(constraints)
@@ -171,8 +172,50 @@ let toggleMic = async () => {
     }
 }
 
+function dragElement(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    // if (document.getElementById(elmnt.id + "header")) {
+    //   /* if present, the header is where you move the DIV from:*/
+    //   document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+    // } else {
+    //   /* otherwise, move the DIV from anywhere inside the DIV:*/
+      elmnt.onmousedown = dragMouseDown;
+    // }
+  
+    function dragMouseDown(e) {
+      e = e || window.event;
+      e.preventDefault();
+      // get the mouse cursor position at startup:
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      document.onmouseup = closeDragElement;
+      // call a function whenever the cursor moves:
+      document.onmousemove = elementDrag;
+    }
+  
+    function elementDrag(e) {
+      e = e || window.event;
+      e.preventDefault();
+      // calculate the new cursor position:
+      pos1 = pos3 - e.clientX;
+      pos2 = pos4 - e.clientY;
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      // set the element's new position:
+      elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+      elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+  
+    function closeDragElement() {
+      /* stop moving when mouse button is released:*/
+      document.onmouseup = null;
+      document.onmousemove = null;
+    }
+  }
+
 window.addEventListener ("beforeunload", leaveChannel);
 document.getElementById ("camera-btn").addEventListener ("click", toggleCamera);
 document.getElementById ("mic-btn").addEventListener ("click", toggleMic);
+
 
 init ();
